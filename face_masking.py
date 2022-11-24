4ac89c005ce277dd6a5c5283e537d69f11961672
icecream126 <kim123hyomin@gmail.com>
2022-11-24 13:51:39 +0900

    Add face cropping

--- face_masking.py
+++ face_masking.py
@@ -4,8 +4,6 @@ import numpy as np
 import copy
 
 
-
-
 def face_masking(img_path):
 
     # Get image
@@ -13,16 +11,19 @@ def face_masking(img_path):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # to show image with matplotlib, we should convert bgr -> rgb
     
     # Set subplot settings (size, location, and title)
-    f = plt.figure(figsize=(10,3))
-    ax1 = f.add_subplot(131)
+    f = plt.figure(figsize=(13,3))
+    ax1 = f.add_subplot(141)
     ax1.set_title("Original Image")
-    ax2 = f.add_subplot(132)
+    ax2 = f.add_subplot(142)
     ax2.set_title("Masked Image")
-    ax3 = f.add_subplot(133)
+    ax3 = f.add_subplot(143)
     ax3.set_title("Face Histogram")
+    ax4=f.add_subplot(144)
+    ax4.set_title('Cropped Image')
     
     # Show original image
     ax1.imshow(img)
+    
     
     # Get image width, height and channel
     img_width = img.shape[1]
@@ -53,6 +54,8 @@ def face_masking(img_path):
     extent = ax3.get_window_extent().transformed(f.dpi_scale_trans.inverted())
     f.savefig('3.jpg',bbox_inches=extent)
     
+    crop_img = copy.deepcopy(img) # copy image for image crop
+    
     ########## Image Masking #################
 
     # 1. Convert RGB to YCbCr
@@ -77,8 +80,8 @@ def face_masking(img_path):
                 img[i][j][2]=255
             else:
                 img[i][j][0]=0
-                img[i][j][1]=1
-                img[i][j][2]=2
+                img[i][j][1]=0
+                img[i][j][2]=0
     
     # Plot masked image
     ax2.imshow(img)
@@ -86,4 +89,58 @@ def face_masking(img_path):
     # Save masked image as jpg
     cv2.imwrite('./2.jpg',img)
     
+    ########### Face Cropping ###############
+    # Get Region of Face
+    # 1. Calculate top, bottom, leftmost, rightmost of face region
+    # 2. Crop image = img[top:bottom, leftmost:rightmost]
+    
+    top_found=False
+    left_most = img_width
+    right_most=0
+    
+    top=img_height
+    bottom=0
+    leftmost=img_width
+    rightmost=0
+    
+    for i in range(img_height):
+        leftmost_found=False
+        for j in range(img_width):
+            
+            # If face region found
+            # Face region of row i-th row, j-th column
+            if img[i][j][0]==255 :
+                # top region is i value of first face region
+                if top_found==False and i!=0:
+                    top=i
+                    top_found=True
+                # leftmost region takes j-th starting point of row and minimum of them
+                if leftmost_found==False and j!=0:
+                    leftmost = min(leftmost,j)
+                    leftmost_found=True
+                
+                if j!=img_width:
+                    rightmost = max(rightmost, j)
+                if i!=img_height:
+                    bottom = max(bottom, i)
+            
+    
+    top = top-20 if top-20>0 else top
+    bottom = bottom+20 if bottom+20<img_height else bottom
+    leftmost = leftmost-20 if leftmost-20>0 else leftmost
+    rightmost = rightmost+20 if rightmost+20<img_width else rightmost
+    
+    # Crop image based on top, bottom, leftmost, rightmost index
+    crop_img = crop_img[top:bottom, leftmost:rightmost]
+    
+    
+    # Show cropped image
+    ax4.imshow(crop_img)
+    
+    # Save cropped image
+    extent = ax4.get_window_extent().transformed(f.dpi_scale_trans.inverted())
+    f.savefig('5.jpg',bbox_inches=extent)
+    
     plt.show()
+    
+face_masking('C:\\DIP\\Face_Reading_AI\\data\\face_yellow.jpg')
